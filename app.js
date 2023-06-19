71d2866dee8a929f2680e1dcdcba68d26acf0810
tsaiiii08 <lintsaiting0824@gamil.com>
2023-06-19 14:06:36 +0800

    add produce random code function

--- app.js
+++ app.js
@@ -23,3 +23,38 @@ app.get('/URL-shortener',(req,res)=>{
 app.listen(3000,()=>{
   console.log('app is runnung on port 3000')
 })
+
+
+
+
+const upperChars = []
+const lowerChars = []
+const arrNum = [0,1,2,3,4,5,6,7,8,9]
+
+// A ~ Z 的編碼是 65 ~ 90
+for (let i = 65; i < 91; i++) {
+  let char = String.fromCharCode(i)
+  upperChars.push(char)
+  lowerChars.push(char.toLowerCase())
+}
+const arrChar = upperChars.concat(lowerChars)
+
+
+function randomChar(){
+  let randomStr = []
+  const numOfNum = (Math.floor(Math.random()*4))+1 //數字介於一到四個之間
+
+  for (let i = 1; i <= 5-numOfNum; i++) {
+    let randomChar = arrChar[Math.floor(Math.random() * 52)]
+    randomStr.push(randomChar)
+  }
+  for (let i = 1; i <=numOfNum; i++) {
+    let randomNum = arrNum[Math.floor(Math.random() * 10)]
+    randomStr.splice((Math.floor(Math.random() * (5 - numOfNum))), 0, randomNum)
+  }
+
+  return randomStr.join('')
+  
+}
+
+
