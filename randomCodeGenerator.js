a02c8fa21336e6423926d6a904af19306a895a79
tsaiiii08 <lintsaiting0824@gamil.com>
2023-07-02 14:57:48 +0800

    modify: edit name of variable in randomCodeGenerator.js

--- randomCodeGenerator.js
+++ randomCodeGenerator.js
@@ -1,11 +1,11 @@
-const arr = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+const sourceString = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 const Url = require('./models/URL-shortener')
 
 function randomCodeGenerator() {
   let randomStr = ""
   for (let i = 0; i < 5 ; i++) {
     let index = Math.floor(Math.random() * 62)
-    randomStr +=  arr[index]
+    randomStr += sourceString[index]
   }
 
   //若該隨機字串已存在於資料庫中，則再次呼叫函示重新產生新的字串
